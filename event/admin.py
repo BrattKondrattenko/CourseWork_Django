f673e7b9eef988f6434ac359182add3ee1440563
BrattKondrattenko <artkondratenko3@gmail.com>
2024-01-09 15:03:50 +0300

    complete excel export

--- event/admin.py
+++ event/admin.py
@@ -1,9 +1,12 @@
+import datetime
+
 from django.contrib import admin
 
 # Register your models here.
 
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from import_export.resources import ModelResource
 
 from event.models import Ticket, Event
 from order.models import Order, Payment
@@ -14,15 +17,32 @@ class TicketInline(admin.StackedInline):
     extra = 0
 
 
+class EventResource(ModelResource):
+    class Meta:
+        model = Event
+        fields = ('name', 'date', 'location')
+
+    def dehydrate_date(self, event):
+        return f"{event.date}"
+
+    def get_location(self, value):
+        return value
+
+
 # Register your models here.
 @admin.register(Event)
 class EventAdmin(ImportExportModelAdmin):
+    resource_class = EventResource
+
     list_display = ('name', 'date', "location")
     list_display_links = ('name',)
     search_fields = ('name',)
     date_hierarchy = "date"
     list_filter = ("date", "location",)
     inlines = [TicketInline]
+
+    def get_export_queryset(self, request):
+        return Event.objects.filter(date__gte=datetime.datetime.today())
 
 
 @admin.register(Ticket)
